ef39085a036898efe94fdfe093217734714e2128
Eightaan <57832730+Eightaan@users.noreply.github.com>
2020-07-28 22:20:48 +0200

    fixes crash with combo counter

--- lua/objectives.lua
+++ lua/objectives.lua
@@ -2,7 +2,6 @@
 if not HMH:GetOption("objective") then return end
 
 Hooks:PostHook(HUDObjectives, "init", "HMH_hudobjectives_init", function(self, hud, ...)
-
 	self._hud_panel = hud.panel
 	if self._hud_panel:child("objectives_panel") then
 		self._hud_panel:remove(self._hud_panel:child("objectives_panel"))
@@ -29,7 +28,12 @@ Hooks:PostHook(HUDObjectives, "init", "HMH_hudobjectives_init", function(self, h
 		x = 0,
 		valign = "top"
 	})	
-
+	self._bg_box = HUDBGBox_create(objectives_panel, {
+		w = 200,
+		x = 26,
+		h = 38,
+		y = 0
+	})
 	local objective_text = objectives_panel:text({
 		y = 8,
 		name = "objective_text", 
@@ -43,7 +47,6 @@ Hooks:PostHook(HUDObjectives, "init", "HMH_hudobjectives_init", function(self, h
 		font_size = tweak_data.hud.active_objective_title_font_size,
 		font = tweak_data.hud.medium_font_noshadow
     })
-	
 	local amount_text = objectives_panel:text({
 	    y = 0, 
     	name = "amount_text",
@@ -58,27 +61,22 @@ Hooks:PostHook(HUDObjectives, "init", "HMH_hudobjectives_init", function(self, h
     	font = tweak_data.hud.medium_font_noshadow
     })   
 
-	   	icon_objectivebox:set_color(Color("ff80df"))
-			
-	    objective_text:set_x(34)
-		objective_text:set_y(0)
-		objective_text:set_font_size(24)
-		objective_text:set_color(Color("66ffff"))
-		objective_text:set_visible(true)
-		
-		amount_text:set_color(Color("ffcc66"))
-	
+	icon_objectivebox:set_color(Color("ff80df"))	
+	objective_text:set_x(34)
+	objective_text:set_y(0)
+	objective_text:set_font_size(24)
+	objective_text:set_color(Color("66ffff"))
+	objective_text:set_visible(true)
+	amount_text:set_color(Color("ffcc66"))
 end)
 
 function HUDObjectives:activate_objective( data )
 	self._active_objective_id = data.id
 	local objectives_panel = self._hud_panel:child( "objectives_panel" )
 	local objective_text = objectives_panel:child( "objective_text" )
-	
+	local _, _, w, _ = objective_text:text_rect()		
 	objective_text:set_text( utf8.to_upper( data.text ) )
-	
-	local _, _, w, _ = objective_text:text_rect()
-	
+		
 	if data.amount then
 		self:update_amount_objective( data )
 	end
@@ -97,7 +95,6 @@ function HUDObjectives:activate_objective( data )
 end
 
 function HUDObjectives:complete_objective(data)
-
 	if data.id ~= self._active_objective_id then
 		return
 	end
@@ -107,7 +104,6 @@ function HUDObjectives:complete_objective(data)
 end
 
 function HUDObjectives:remind_objective(id)
-
 	if id ~= self._active_objective_id then
 		return
 	end
