ded41d0a418f12e4f42ff29eb0d0b3777ff53489
Eightaan <57832730+Eightaan@users.noreply.github.com>
2020-08-26 00:47:04 +0200

    Update hint.lua

--- lua/hint.lua
+++ lua/hint.lua
@@ -1,6 +1,6 @@
 Hooks:PostHook(HUDHint, "init", "hudhint_HMH", function(self, ...)
 	local clip_panel = self._hint_panel:child("clip_panel")
-	
+
 	if HMH:GetOption("hint") then
 	    clip_panel:child("bg"):hide()
 	    clip_panel:child("hint_text"):set_font(Idstring("fonts/font_medium"))
