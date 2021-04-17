06dbc923f463a454a1e7c1698a9aa21f30a1ea48
Eightaan <57832730+Eightaan@users.noreply.github.com>
2021-04-17 13:36:25 +0200

    Add files via upload

--- lua/TeamPanel.lua
+++ lua/TeamPanel.lua
@@ -127,7 +127,7 @@ function HUDTeammate:set_voice_com(status)
 	local texture = status and "guis/textures/pd2/jukebox_playing" or "guis/textures/pd2/hud_tabs"
 	local texture_rect = status and { 0, 0, 16, 16 } or { 84, 34, 19, 19 }
 	local callsign = self._panel:child("callsign")
-	callsign:set_image(texture, unpack(texture_rect))
+	if HMH:GetOption("voice") then callsign:set_image(texture, unpack(texture_rect)) end
 end
 
 if HMH:GetOption("interact_info") or HMH:GetOption("color_name") then
