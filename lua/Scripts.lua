f71fc1282f5ec0ac31231bee0f2533a68516fcef
Eightaan <57832730+Eightaan@users.noreply.github.com>
2021-09-11 10:18:11 +0200

    Add files via upload

--- lua/Scripts.lua
+++ lua/Scripts.lua
@@ -218,8 +218,9 @@ elseif RequiredScript == "lib/managers/hud/hudhitconfirm" then
 		    if self._hud_panel:child("headshot_confirm") then
 		    	self._hud_panel:remove(self._hud_panel:child("headshot_confirm"))
 		    end
+			local headshot_icon = HMH:GetOption("headshot_texture") == 3 and "guis/textures/pd2_mod_hmh/Headshot_confirm_blessings" or "guis/textures/pd2_mod_hmh/Headshot_confirm"
 		    self._headshot_confirm = self._hud_panel:bitmap({
-				texture = "guis/textures/pd2_mod_hmh/Headshot_confirm",
+				texture = headshot_icon,
 			    name = "headshot_confirm",
 			    halign = "center",
 			    visible = false,
@@ -242,7 +243,7 @@ elseif RequiredScript == "lib/managers/hud/hudhitconfirm" then
 elseif RequiredScript == "lib/managers/playermanager" then
 	Hooks:PostHook(PlayerManager, "on_headshot_dealt", "hmh_on_headshot_dealt", function(self, ...)
 		if not VHUDPlus then
-	        if HMH:GetOption("headshot_mark") then
+	        if HMH:GetOption("headshot_texture") == 2 or 3 then
 		        managers.hud:on_headshot_confirmed()
 		    end
 		end
