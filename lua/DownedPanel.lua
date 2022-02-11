f5b237c927153a8e0e142a1a310a48b374dde1d7
Eightaan <57832730+Eightaan@users.noreply.github.com>
2022-02-11 15:54:21 +0100

    Add files via upload

--- lua/DownedPanel.lua
+++ lua/DownedPanel.lua
@@ -3,9 +3,10 @@ if not HMH:GetOption("downed") then
 end
 
 Hooks:PostHook(HUDPlayerDowned, "init", "hudplayerdowned_HMH", function(self, ...)
-    if self._hud_panel:child("downed_panel"):child("timer_msg") then
-	    self._hud_panel:child("downed_panel"):child("timer_msg"):set_color(HMH:GetColor("DownedText"))
-    	self._hud_panel:child("downed_panel"):child("timer_msg"):set_alpha(HMH:GetOption("DownedAlpha"))
+	local timer_msg = self._hud_panel:child("downed_panel"):child("timer_msg")
+    if timer_msg then
+	    timer_msg:set_color(HMH:GetColor("DownedText"))
+        timer_msg:set_alpha(HMH:GetOption("DownedAlpha"))
 	end
 	self._hud.timer:set_color(HMH:GetColor("DownedTimer"))
 	self._hud.timer:set_alpha(HMH:GetOption("DownedAlpha"))
@@ -13,17 +14,18 @@ Hooks:PostHook(HUDPlayerDowned, "init", "hudplayerdowned_HMH", function(self, ..
 end)
 
 Hooks:PostHook(HUDPlayerDowned, "show_timer", "hudplayerdowned_show_timer_HMH", function(self, ...)
-	local downed_panel = self._hud_panel:child("downed_panel")
-	local timer_msg = downed_panel:child("timer_msg")
+	local timer_msg = self._hud_panel:child("downed_panel"):child("timer_msg")
 
-	timer_msg:set_alpha(HMH:GetOption("DownedAlpha"))
+    if timer_msg then
+	    timer_msg:set_alpha(HMH:GetOption("DownedAlpha"))
+	end
 	self._hud.timer:set_alpha(HMH:GetOption("DownedAlpha"))
 end)
 
 Hooks:PostHook(HUDPlayerDowned, "hide_timer", "hudplayerdowned_hide_timer_HMH", function(self, ...)
-	local downed_panel = self._hud_panel:child("downed_panel")
-	local timer_msg = downed_panel:child("timer_msg")
-
-	timer_msg:set_alpha(HMH:GetOption("DownedAlpha") / 2)
+	local timer_msg = self._hud_panel:child("downed_panel"):child("timer_msg")
+    if timer_msg then
+	    timer_msg:set_alpha(HMH:GetOption("DownedAlpha"))
+	end
 	self._hud.timer:set_alpha(HMH:GetOption("DownedAlpha") / 2)
 end)
--- lua/ObjectivesPanel.lua
+++ lua/ObjectivesPanel.lua
@@ -2,76 +2,7 @@ if not HMH:GetOption("objective") then
     return
 end
 
-Hooks:PostHook(HUDObjectives, "init", "HMH_hudobjectives_init", function(self, hud, ...)
-	self._hud_panel = hud.panel
-	if self._hud_panel:child("objectives_panel") then
-		self._hud_panel:remove(self._hud_panel:child("objectives_panel"))
-	end
-	local objectives_panel = self._hud_panel:panel({
-		y = 0,
-		name = "objectives_panel",
-		h = 100,
-		visible = false,
-		w = 500,
-		x = 0,
-		valign = "top"
-	})
-	local icon_objectivebox = objectives_panel:bitmap({
-		texture = "guis/textures/pd2/hud_icon_objectivebox",
-		name = "icon_objectivebox",
-		h = 24,
-		layer = 0,
-		w = 24,
-		y = 0,
-		visible = true,
-		blend_mode = "normal",
-		halign = "left",
-		x = 0,
-		valign = "top"
-	})
-	self._bg_box = HUDBGBox_create(objectives_panel, {
-		w = 200,
-		x = 26,
-		h = 38,
-		y = 0
-	})
-	local objective_text = objectives_panel:text({
-		y = 8,
-		name = "objective_text",
-		vertical = "top",
-		align = "left",
-		text = "",
-		visible = false,
-		x = 0,
-		layer = 2, 
-		color = Color.white,
-		font_size = tweak_data.hud.active_objective_title_font_size,
-		font = tweak_data.hud.medium_font_noshadow
-    })
-	local amount_text = objectives_panel:text({
-	    y = 0,
-    	name = "amount_text",
-	    vertical = "top",
-		align = "left",
-		text = "1/4",
-		visible = true,
-        x = 6,
-    	layer = 2,
-		color = Color.white,
-		font_size = tweak_data.hud.active_objective_title_font_size,
-    	font = tweak_data.hud.medium_font_noshadow
-    })
-	icon_objectivebox:set_color(HMH:GetColor("ObjectiveIcon"))
-	icon_objectivebox:set_alpha(HMH:GetOption("objective_text"))
-	objective_text:set_x(34)
-	objective_text:set_alpha(HMH:GetOption("objective_text"))
-	objective_text:set_y(0)
-	objective_text:set_font_size(24)
-	objective_text:set_color(HMH:GetColor("ObjectiveText"))
-	objective_text:set_visible(true)
-	amount_text:set_color(HMH:GetColor("ObjectiveAmount"))
-	amount_text:set_alpha(HMH:GetOption("objective_text"))
-end)
+
 
 function HUDObjectives:activate_objective( data )
 	self._active_objective_id = data.id
