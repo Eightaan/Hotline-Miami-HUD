0590ff27cc88ad523a6163f4e4ab8b0a659eab9e
Eightaan <57832730+Eightaan@users.noreply.github.com>
2023-03-01 11:33:12 +0100

    Delete Override.lua

--- Override.lua
+++
@@ -1,4 +0,0 @@
-if file.DirectoryExists("assets/mod_overrides/Hotline Miami Menu") and (HMH:GetOption("preset") == 3 or not HMH:GetOption("no_menu_textures")) then
-	SystemFS:delete_file("assets/mod_overrides/Hotline Miami Menu")
-	Override = true
-end
