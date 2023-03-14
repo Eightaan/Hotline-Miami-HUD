d84de3c9e837f19dc6e9a9742a5a9f1cd00f9807
Eightaan <57832730+Eightaan@users.noreply.github.com>
2023-03-14 01:16:51 +0100

    Add files via upload

--- lua/ECMTimer.lua
+++ lua/ECMTimer.lua
@@ -2,9 +2,7 @@ if RequiredScript == "lib/managers/hudmanagerpd2" then
 	HUDECMCounter = HUDECMCounter or class()
 
     function HUDECMCounter:init(hud)
-		self._end_time = 0
-		self._nonpager_end_time = 0
-		
+		self._ecm_timer = 0
 	    self._hud_panel = hud.panel
 	    self._ecm_panel = self._hud_panel:panel({
 		    name = "ecm_counter_panel",
@@ -13,12 +11,10 @@ if RequiredScript == "lib/managers/hudmanagerpd2" then
 		    w = 200,
 		    h = 200
 	    })
-
 	    self._ecm_panel:set_top(50)
         self._ecm_panel:set_right(self._hud_panel:w() + 11)
 
 	    local ecm_box = HUDBGBox_create(self._ecm_panel, { w = 38, h = 38, },  {})
-		
 		if HMH:GetOption("assault") or HMH:GetOption("hide_hudbox") then
 		   ecm_box:child("bg"):hide()
 		   ecm_box:child("left_top"):hide()
@@ -54,76 +50,37 @@ if RequiredScript == "lib/managers/hudmanagerpd2" then
 	    ecm_icon:set_right(ecm_box:parent():w())
 	    ecm_icon:set_center_y(ecm_box:h() / 2)
 		ecm_box:set_right(ecm_icon:left())
-        
-		local pagers_texture, pagers_rect = tweak_data.hud_icons:get_icon_data("pagers_used")
-		local pager_icon = self._ecm_panel:bitmap({
-		    name = "pager_icon",
-		    texture = pagers_texture,
-		    texture_rect = pagers_rect,
-		    valign = "top",
-			visible = false,
-			color = HMH:GetColor("ECMUpgradeIcon"),
-		    layer = 2,
-		    w = ecm_box:w() / 2,
-		    h = ecm_box:h() / 2
-	    })
-		pager_icon:set_right(self._ecm_panel:w() - 20 )
-		pager_icon:set_center_y(ecm_box:h())
-
-		self._active_ecm = false
-		self._pocket_ecm = false
-		self._pager_block = false
     end
-
+	
     function HUDECMCounter:update()
 		local current_time = TimerManager:game():time()
-		local t = self._end_time - current_time
-		if t < 0 then
-			t = self._nonpager_end_time - current_time
-			if t > 0 then
-				self._end_time = self._nonpager_end_time
-				self._pocket_ecm = false
-				managers.hud:update_ecm_icons(false)
-			end
-		end
+		local t = self._ecm_timer - current_time
 		if managers.groupai:state():whisper_mode() then
 			self._ecm_panel:set_visible(t > 0)
-			
 			if t > 0.1 then
 			    local t_format = t < 10 and "%.1fs" or "%.fs"
-				self._active_ecm = true
 				self._text:set_text(string.format(t_format, t))
-				
 			    if t < 3 then
 				    self._text:set_color(HMH:GetColor("ecm_low"))
 				    self._text:animate(function(o)
-                    over(1 , function(p)
-					    t = t + coroutine.yield()
-						local font = tweak_data.hud_corner.numhostages_size * 0.9
-                        local n = 1 - math.sin(t * 700)
-                        self._text:set_font_size( math.lerp(font , (font) * 1.05, n))
-                    end)
-                end)
-
-			    elseif t < 9.9 then
-				    self._text:stop()
-				    self._text:set_color(HMH:GetColor("ecm_mid"))
+						over(1 , function(p)
+							t = t + coroutine.yield()
+							local font = tweak_data.hud_corner.numhostages_size * 0.9
+							local n = 1 - math.sin(t * 700)
+							self._text:set_font_size( math.lerp(font , (font) * 1.05, n))
+						end)
+					end)
+				elseif t < 9.9 then
+					self._text:stop()
+					self._text:set_color(HMH:GetColor("ecm_mid"))
 			    else
 				    self._text:stop()
 				    self._text:set_color(HMH:GetColor("ECMText"))
 			    end
-			else
-				self._active_ecm = false
 			end
 		else
 			self._ecm_panel:set_visible(false)
 		end
-    end
-
-	function HUDECMCounter:update_icons(jam_pagers)
-        local pager_icon = self._ecm_panel:child("pager_icon")
-        pager_icon:set_visible(jam_pagers and HMH:GetOption("pager_jam"))
-		self._pager_block = jam_pagers
     end
 
 	--Init
@@ -136,63 +93,76 @@ if RequiredScript == "lib/managers/hudmanagerpd2" then
 		self._hud_ecm_counter:update()
 	end)
 
-	function HUDManager:update_ecm_icons(jam_pagers)
-        self._hud_ecm_counter:update_icons(jam_pagers)
-    end
-
 elseif RequiredScript == "lib/units/equipment/ecm_jammer/ecmjammerbase" then
-	
-	Hooks:PostHook(ECMJammerBase, "setup", "hmh_ECMJammerBase_setup", function(self, battery_life_upgrade_lvl, ...)
-		local new_end_time = TimerManager:game():time() + self:battery_life()
-		if new_end_time > managers.hud._hud_ecm_counter._end_time then
-			if battery_life_upgrade_lvl ~= 3 then
-				managers.hud._hud_ecm_counter._nonpager_end_time = new_end_time
-				if (managers.hud._hud_ecm_counter._pager_block == false or managers.hud._hud_ecm_counter._active_ecm == false) then
-					managers.hud._hud_ecm_counter._end_time = new_end_time
-					managers.hud:update_ecm_icons(battery_life_upgrade_lvl == 3)
-				end
-			else
-				managers.hud._hud_ecm_counter._end_time = new_end_time
-				managers.hud:update_ecm_icons(battery_life_upgrade_lvl == 3)
-			end
-			managers.hud._hud_ecm_counter._pocket_ecm = false
-		end
-	end)
-	
-	Hooks:PostHook(ECMJammerBase, "sync_setup", "hmh_ECMJammerBase_sync_setup", function(self, upgrade_lvl, ...)
-		local new_end_time = TimerManager:game():time() + self:battery_life()
-		if new_end_time > managers.hud._hud_ecm_counter._end_time then
-			if upgrade_lvl ~= 3 then
-				managers.hud._hud_ecm_counter._nonpager_end_time = new_end_time
-				if (managers.hud._hud_ecm_counter._pager_block == false or managers.hud._hud_ecm_counter._active_ecm == false) then
-					managers.hud._hud_ecm_counter._end_time = new_end_time
-					managers.hud:update_ecm_icons(upgrade_lvl == 3)
-				end
-			else
-				managers.hud._hud_ecm_counter._end_time = new_end_time
-				managers.hud:update_ecm_icons(upgrade_lvl == 3)
-			end
-			managers.hud._hud_ecm_counter._pocket_ecm = false
-		end
-	end)
+	--PeerID
+	local original =
+	{
+		spawn = ECMJammerBase.spawn,
+		set_server_information = ECMJammerBase.set_server_information,
+		set_owner = ECMJammerBase.set_owner,
+		sync_setup = ECMJammerBase.sync_setup
+	}
 
-	Hooks:PostHook(ECMJammerBase, "update", "hmh_ECMJammerBase_update", function(self, unit, t, ...)
-		if (managers.hud._hud_ecm_counter._end_time == 0) then
-			local new_end_time = TimerManager:game():time() + self:battery_life()
-			managers.hud._hud_ecm_counter._end_time = new_end_time
-			managers.hud:update_ecm_icons(false)
-			managers.hud._hud_ecm_counter._active_ecm = true
-			managers.hud._hud_ecm_counter._pocket_ecm = false
+	function ECMJammerBase.spawn(pos, rot, battery_life_upgrade_lvl, owner, peer_id, ...)
+		local unit = original.spawn(pos, rot, battery_life_upgrade_lvl, owner, peer_id, ...)
+		unit:base():SetPeersID(peer_id)
+		return unit
+	end
+
+	function ECMJammerBase:set_server_information(peer_id, ...)
+		original.set_server_information(self, peer_id, ...)
+		self:SetPeersID(peer_id)
+	end
+
+	function ECMJammerBase:sync_setup(upgrade_lvl, peer_id, ...)
+		original.sync_setup(self, upgrade_lvl, peer_id, ...)
+		self:SetPeersID(peer_id)
+	end
+
+	function ECMJammerBase:set_owner(...)
+		original.set_owner(self, ...)
+		self:SetPeersID(self._owner_id or 0)
+	end
+
+	function ECMJammerBase:SetPeersID(peer_id)
+		local id = peer_id or 0
+		self._hmh_peer_id = id
+		self._hmh_local_peer = id == managers.network:session():local_peer():id()
+	end
+
+	--ECM Timer Host and Client
+	local set_active_original = ECMJammerBase.set_active
+	function ECMJammerBase:set_active(active, ...)
+    set_active_original(self, active, ...)
+		if active then
+		    local battery_life = self:battery_life()
+            if battery_life == 0 then
+                return
+            end
+			local ecm_timer = TimerManager:game():time() + battery_life
+			local jam_pagers = false
+			if self._hmh_local_peer then
+				jam_pagers = managers.player:has_category_upgrade("ecm_jammer", "affects_pagers")
+			elseif self._hmh_peer_id ~= 0 then
+				local peer = managers.network:session():peer(self._hmh_peer_id)
+				if peer and peer._unit and peer._unit.base then
+					jam_pagers = peer._unit:base():upgrade_value("ecm_jammer", "affects_pagers")
+				end
+			end
+			if jam_pagers or not HMH:GetOption("pager_jam") then
+				managers.hud._hud_ecm_counter._ecm_timer = ecm_timer
+			else
+				return
+			end
 		end
-	end)
-	
+	end
+
 elseif RequiredScript == "lib/units/beings/player/playerinventory" then
+	-- Pocket ECM
 	Hooks:PostHook(PlayerInventory, "_start_jammer_effect", "hmh_PlayerInventory__start_jammer_effect", function(self, end_time)
-		local new_end_time = end_time or TimerManager:game():time() + self:get_jammer_time()
-		if new_end_time > managers.hud._hud_ecm_counter._end_time or managers.hud._hud_ecm_counter._pager_block == false then
-			managers.hud._hud_ecm_counter._end_time = new_end_time
-			managers.hud:update_ecm_icons(true)
-			managers.hud._hud_ecm_counter._pocket_ecm = true
+		local ecm_timer = end_time or TimerManager:game():time() + self:get_jammer_time()
+		if ecm_timer > managers.hud._hud_ecm_counter._ecm_timer then
+			managers.hud._hud_ecm_counter._ecm_timer = ecm_timer
 		end
 	end)
 end
