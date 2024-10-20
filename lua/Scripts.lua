c1851a45ead73fccc80120f080d7aa8b5e23b3e6
Eightaan <57832730+Eightaan@users.noreply.github.com>
2024-10-20 19:41:54 +0200

    Delete lua/Scripts.lua

--- lua/Scripts.lua
+++
@@ -1,7 +0,0 @@
-local PlayerDamage_restore_health = PlayerDamage.restore_health
-function PlayerDamage:restore_health(health_restored, ...)
-	if health_restored * self._healing_reduction == 0 then
-		return
-	end
-	return PlayerDamage_restore_health(self, health_restored, ...)
-end
